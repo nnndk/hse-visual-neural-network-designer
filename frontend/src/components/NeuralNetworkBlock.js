352dad45f48d0e6066692d3c4e131354ac0e14bd
Yusupov Ilya <iryusupov@edu.hse.ru>
2024-03-17 16:50:52 +0300

    Added backend service and select nw library

--- frontend/src/components/NeuralNetworkBlock.js
+++ frontend/src/components/NeuralNetworkBlock.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
 import React from 'react';
 import '../styles/NeuralNetworkBlock.css';
+import '../styles/NewNeuralNetworkBlock.css';
 
 const NeuralNetworkBlock = ({ name, parameters, onDuplicate, onEdit, onDelete, editing, editingParams, onSaveEdit, onCancelEdit, onUpdateParams }) => {
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  const handleSaveEdit = () => {
+    const newErrorMessages = editingParams.map(param => {
+      if(`${param.value}`.trim() === '')
+        return `Нет значения у параметра - ${param.name}`;
+      else
+        return '';
+    });
+
+    // Устанавливаем сообщения об ошибках
+    setErrorMessages(newErrorMessages);
+
+    // Проверяем, есть ли пустые поля
+    const isEmptyField = newErrorMessages.some(message => message !== '');
+    if (isEmptyField) {
+      return;
+    }
+
+    // Если все поля заполнены, сохраняем изменения
+    onSaveEdit([...editingParams]);
+  };
+
   return (
     <div className="neural-network-block">
       <div className="block-header">
@@ -10,22 +35,17 @@ const NeuralNetworkBlock = ({ name, parameters, onDuplicate, onEdit, onDelete, e
       <div className="block-content">
         {editing ? (
           <ul>
+            {errorMessages.map((message, index) => (
+              message && <div key={index} className="error">{message}</div>
+            ))}
             {editingParams.map((param, index) => (
               <li key={index}>
-                <input
-                  type="text"
-                  value={param.name}
-                  onChange={(e) => {
-                    const updatedParameters = [...editingParams];
-                    updatedParameters[index].name = e.target.value;
-                    onUpdateParams(updatedParameters);
-                  }}
-                />
-                :
+                {param.name} : 
                 <input
                   type="text"
                   value={param.value}
                   onChange={(e) => {
+                    setErrorMessages([]);
                     const updatedParameters = [...editingParams];
                     updatedParameters[index].value = e.target.value;
                     onUpdateParams(updatedParameters);
@@ -47,8 +67,11 @@ const NeuralNetworkBlock = ({ name, parameters, onDuplicate, onEdit, onDelete, e
       <div className="block-footer">
         {editing ? (
           <>
-            <button onClick={() => onSaveEdit([...editingParams])}>Сохранить</button>
-            <button onClick={() => onCancelEdit()}>Отмена</button>
+            <button onClick={() => handleSaveEdit()}>Сохранить</button>
+            <button onClick={() => {
+              setErrorMessages([]);
+              onCancelEdit();
+            }}>Отмена</button>
           </>
         ) : (
           <>
---
+++ frontend/src/services/environment.js
@@ -0,0 +1,1 @@
+export const localBaseUrl = "http://localhost:8000"
